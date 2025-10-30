050ddcaed4c4068ed8ad96ac18ac938a50c9eac7
Eldor Asher <eldorasher@MacBook-Air-sl-Eldor.local>
2025-10-30 18:26:07 +0200

    done

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,12 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+		int correct_value = Integer.parseInt(args[0]);
+		int percentage = Integer.parseInt(args[1]);
+		int year = Integer.parseInt(args[2]);
+		double rate = (double)percentage / 100;
+		percentage = percentage % 100 ;
+		double total = correct_value * Math.pow((1 + rate),year);
+		System.out.println("After " +year+ " years " +correct_value+ " saved at " +percentage+ "% will yield " +(int)total+ " shekels!");
 	}
 }
